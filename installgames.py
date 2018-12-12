3d1c73a731a054bb7ce238e2db92cd0f38caea21
cdav1601 <45018911+cdav1601@users.noreply.github.com>
2018-12-12 16:03:57 -0500

    for executing games as modules

---
+++ installgames.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+
+import os.path
+import games
+from games import *
+
+
+
+#Provides list of games in package to populate main menu
+def gamelist():
+
+	#Add name of game as it should appear in the menu
+	return(['Hangman'])
+gamelist.__doc__ = """List of available games"""
+
+
+
+#Executes Hangman game
+def hangmanexec():
+
+	#print("hangman")
+	return hangman.main()
+hangman.__doc__ = """Executes Hangman game"""
+
+
+
+if __name__ == '__main__':
+    assert hangmanexec() == True
