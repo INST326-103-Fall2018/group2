7bb7e8835830f78348cdf79fef2f3c479db96a47
cdav1601 <45018911+cdav1601@users.noreply.github.com>
2018-12-15 12:52:57 -0500

    Modified to work with package

--- rolling_dice.py
+++ rolling_dice.py
@@ -1,16 +1,26 @@
-#Name: Adrian
+#Adrian
+
 
 import random
-min = 1
-max = 6
 
-roll_again = "yes"
+def main():
+	min = 1
+	max = 6
 
-while roll_again == "yes" or roll_again == "y":
-    print "Rolling the dices..."
-    print "The values are...."
-    print random.randint(min, max)
-    print random.randint(min, max)
+	roll_again = "yes"
 
-    roll_again = raw_input("Roll the dices again?")
+	while roll_again == "yes" or roll_again == "y":
+	    print("Rolling the dices...")
+	    print("The values are....")
+	    print(random.randint(min, max))
+	    print(random.randint(min, max))
 
+	    roll_again = input("Roll the dices again?")
+
+
+	return "q", 0, 0
+
+
+
+if __name__ == '__main__':
+    main()
